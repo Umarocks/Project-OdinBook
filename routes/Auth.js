9d8e424d5f37e0360efa64f9d25826e94f89f2e7
Umar Aziz <umarocks@hotmail.com>
2024-02-14 13:08:24 -0600

    Auth using Bycrypt and moved login and register to one auth route

---
+++ routes/Auth.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../Schema/user");
+const passport = require("passport");
+const { hashPassword } = require('../utils/helper');
+
+router.post("/register",async (req, res) => {
+  const { username, password, email, firstName, lastName, membershipStatus } =
+    req.body;
+  const newUser = new User({
+    username: username,
+    password: hashPassword(password),
+    email: email,
+    firstName: firstName,
+    lastName: lastName,
+    membershipStatus: membershipStatus,
+  });
+  const userExist = await User.findOne({
+    $or: [{ username: username }, { email: email }],
+    });
+  if (!userExist) {
+    await newUser.save();
+    console.log("newUser");
+    
+  } else {
+    console.log("userExist");
+    console.log(userExist);
+  }
+  res.send("register post");
+});
+
+
+router.get("/login", passport.authenticate('local'), async (req, res) => {
+  const { username, password, email, firstName, lastName, membershipStatus } =
+    req.body;
+
+  const newUser = new User({
+    username: username,
+    password: password,
+    email: email,
+    firstName: firstName,
+    lastName: lastName,
+    membershipStatus: membershipStatus,
+  });
+  
+  console.log(userExist);
+  if (!userExist) {
+    res.write("User not found");
+    console.log("User Not Found");
+  } else {
+    res.write("User found");
+    console.log("Login Successful");
+  }
+  res.send();
+});
+
+
+module.exports = router;
