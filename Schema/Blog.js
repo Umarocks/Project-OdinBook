ee574fa65ec22046f39cc950bcd1db16c183c0b7
Umar Aziz <umarocks@hotmail.com>
2024-02-25 13:03:26 -0600

    identation fixes.

--- Schema/Blog.js
+++ Schema/Blog.js
@@ -8,7 +8,7 @@ const blogSchema = new mongoose.Schema(
     },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
 messageSchema.virtual("when").get(function () {
